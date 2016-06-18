56610471c3838d195f90e8fb23a29dd1bdb88b5b
Zac <mymengyu@gmail.com>
2016-06-18 18:04:10 +0800

    Add sticker id for initialize

--- lib/messenger/parameters/message.rb
+++ lib/messenger/parameters/message.rb
@@ -3,11 +3,12 @@ module Messenger
     class Message
       include Callback
 
-      attr_accessor :mid, :seq, :text, :attachments
+      attr_accessor :mid, :seq, :sticker_id, :text, :attachments
 
-      def initialize(mid:, seq:, text: nil, attachments: nil)
+      def initialize(mid:, seq:, sticker_id: nil, text: nil, attachments: nil)
         @mid         = mid
         @seq         = seq
+        @sticker_id  = sticker_id if sticker_id.present?
         @text        = text if text.present?
         @attachments = build_attachments(attachments) if attachments.present?
       end
