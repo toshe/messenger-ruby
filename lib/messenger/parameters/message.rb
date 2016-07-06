e6f6c8a4b997c4adc63b3b805f85834d4a153fe3
mejcz <magda.wisniewska@netguru.co>
2016-07-06 16:48:23 +0200

    Allow additional Message params: is_echo, app_id and metadata
    Facebook’s added 3 additional (optional) args

--- lib/messenger/parameters/message.rb
+++ lib/messenger/parameters/message.rb
@@ -3,14 +3,17 @@ module Messenger
     class Message
       include Callback
 
-      attr_accessor :mid, :seq, :sticker_id, :text, :attachments
+      attr_accessor :mid, :seq, :sticker_id, :text, :attachments, :is_echo, :app_id, :metadata
 
-      def initialize(mid:, seq:, sticker_id: nil, text: nil, attachments: nil)
+      def initialize(mid:, seq:, sticker_id: nil, text: nil, attachments: nil, is_echo: nil, app_id: nil, metadata: nil)
         @mid         = mid
         @seq         = seq
         @sticker_id  = sticker_id if sticker_id.present?
         @text        = text if text.present?
         @attachments = build_attachments(attachments) if attachments.present?
+        @is_echo     = is_echo
+        @app_id      = app_id
+        @metadata    = metadata
       end
 
       def build_attachments(attachments)
