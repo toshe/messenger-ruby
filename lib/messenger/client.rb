a5c7536fb12f7917381f7625bd686098384fb737
Todor Lazov <todor@lazov.com>
2016-09-16 09:52:33 +0200

    Added the ability to change the page_access_token on per-request basis

--- lib/messenger/client.rb
+++ lib/messenger/client.rb
@@ -1,15 +1,15 @@
 module Messenger
   class Client
-    def self.get_user_profile(user_id)
+    def self.get_user_profile(user_id, page_access_token=Messenger.config.page_access_token)
       JSON.parse(RestClient.get(
         "https://graph.facebook.com/v2.6/#{user_id}?fields=first_name,last_name,profile_pic \
-        &access_token=#{Messenger.config.page_access_token}",
+        &access_token=#{page_access_token}",
       ))
     end
 
-    def self.send(data)
+    def self.send(data, page_access_token=Messenger.config.page_access_token)
       RestClient.post(
-        "https://graph.facebook.com/v2.6/me/messages?access_token=#{Messenger.config.page_access_token}",
+        "https://graph.facebook.com/v2.6/me/messages?access_token=#{page_access_token}",
         data.build.to_json,
         content_type: :json
       )
